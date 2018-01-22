0a03b1618177556465d8930a1f8fa0c9e33025e9
collin <p.collinvd@hotmail.com>
2018-01-22 14:27:32 +0100

    Initial commit
    Project from Blackboard
    Also made an initial main class :D

---
+++ Parkeersimulator/Main.java
@@ -0,0 +1,10 @@
+package Parkeersimulator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Backlit keyboard is eigenlijk best fucking vet");
+	}
+
+}
