549f85ff5f75c96495ba21fe562aed57b6837de3
Remi Reuvekamp <git@remireuvekamp.nl>
2018-01-26 15:11:49 +0100

    Make ParkingLot iteratable

--- src/Parkeersimulator/Models/ParkingLot.java
+++ src/Parkeersimulator/Models/ParkingLot.java
@@ -1,6 +1,8 @@
 package Parkeersimulator.Models;
 
+import java.awt.Color;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import Parkeersimulator.view.AbstractView;
@@ -9,7 +11,7 @@ import Parkeersimulator.view.CarParkView;
 /**
  * Hoi dit is een comment
  */
-public class ParkingLot {
+public class ParkingLot implements Iterable<Car> {
 
 	private List<AbstractView> views;
     private CarParkView carParkView;
@@ -173,6 +175,91 @@ public class ParkingLot {
             }
         }
     }
+
+    public CarAmount[] calculateAmountOfCars()
+    {
+    	CarAmount adHoc = new CarAmount();
+    	adHoc.carName = "AdHoc";
+    	adHoc.carColor = (new AdHocCar()).getColor();
+
+    	CarAmount reservedCar = new CarAmount();
+    	reservedCar.carName = "Reserved";
+    	reservedCar.carColor = (new ReservedCar()).getColor();
+
+    	CarAmount parkingPassCar = new CarAmount();
+    	parkingPassCar.carName = "Pass";
+    	parkingPassCar.carColor = (new ParkingPassCar()).getColor();
+
+    	CarAmount[] amounts = new CarAmount[]{
+    		adHoc, reservedCar, parkingPassCar
+    	};
+
+    	// TODO: Loop over cars.
+		for (Car c : this) {
+			if (c instanceof AdHocCar) {
+				amounts[0].amount++;
+			} else if (c instanceof ReservedCar) {
+				amounts[1].amount++;
+			} else if (c instanceof ParkingPassCar) {
+				amounts[2].amount++;
+			}
+
+			System.out.println("Car: " + ((c == null) ? "null" :c.getLocation()));
+		}
+
+		return amounts;
+    }
+
+	@Override
+	public Iterator<Car> iterator()
+	{
+		return new ParkingLotSpotIterator();
+	}
+
+    public class CarAmount
+    {
+    	public String carName;
+    	public Color carColor;
+    	public int amount;
+    }
+
+	public class ParkingLotSpotIterator implements Iterator<Car>
+	{
+		private int curFloor = 0;
+		private int curRow = 0;
+		private int curSpot = 0;
+
+		@Override
+		public boolean hasNext()
+		{
+			int amount = numberOfFloors*numberOfRows*numberOfPlaces;
+
+			int val = this.curFloor*this.curRow*this.curSpot;
+
+			return amount > val;
+		}
+
+		@Override
+		public Car next()
+		{
+			this.curSpot++;
+			if (this.curSpot >= numberOfPlaces) {
+				this.curSpot = 0;
+				this.curRow++;
+			}
+
+			if (this.curRow >= numberOfRows) {
+				this.curRow = 0;
+				this.curFloor++;
+			}
+
+			if (this.curFloor >= numberOfFloors) {
+				return null;
+			}
+
+			return cars[this.curFloor][this.curRow][this.curSpot];
+		}
+	}
 
     /**
      * Check if a location exists; is not outside our parking space.
